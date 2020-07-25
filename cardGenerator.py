c2729056de0cd863546ac1c74bbfbf52143aee89
Sam <samuel_morgan_@outlook.com>
2020-07-25 18:26:06 +0100

    Regex detects time & line

--- cardGenerator.py
+++ cardGenerator.py
@@ -1,4 +1,12 @@
+import re
+
 front = open("FrontSubtitles.txt").read()
 back = open("BackSubtitles.txt").read()
-print(front)
-print(back)
+
+frontMatches = re.findall(r'(\d+ --> \d+)\n(.+?)(?=\d+ --> \d+)', front, flags=re.MULTILINE|re.DOTALL)
+backMatches = re.findall(r'(\d+ --> \d+)\n(.+?)(?=\d+ --> \d+)', back, flags=re.MULTILINE|re.DOTALL)
+
+for match in frontMatches:
+    print(match)
+for match in backMatches:
+    print(match)
