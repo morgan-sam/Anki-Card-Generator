d5dba7ec13421e0fef2fb1f343e600ea596cf078
Sam <samuel_morgan_@outlook.com>
2020-08-09 16:07:13 +0100

    Working dotenv

--- acg.py
+++ acg.py
@@ -1,8 +1,12 @@
-
-from functions.subtitlesConversion import subtitlesToCardArray
-from functions.listConversion import listToCardArray
 from functions.generateCards import generateDeck
+from functions.listConversion import listToCardArray
+from functions.subtitlesConversion import subtitlesToCardArray
+from dotenv import load_dotenv, find_dotenv
+import os
 
+load_dotenv(find_dotenv())
+SECRET_KEY = os.environ.get("SECRET_KEY")
+print(SECRET_KEY)
 
 # cardArray = subtitlesToCardArray()
 cardArray = listToCardArray()
