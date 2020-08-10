26f482b27b28ecf5cec03fbcebafd248e6e46453
Sam <samuel_morgan_@outlook.com>
2020-08-10 21:09:27 +0100

    Lines are correctly formatted

--- functions/listConversion.py
+++ functions/listConversion.py
@@ -5,11 +5,10 @@ from googletrans import Translator
 def listToCardArray():
     translator = Translator()
     translations = []
-    filepath = 'Sentences.txt'
-    with open(filepath) as fp:
-        for line in enumerate(fp):
-            entry = {"front": line[1], "back": translator.translate(
-                line[1], dest='es').text}
-            translations.append(entry)
-    print(translations)
+    file = open('Sentences.txt')
+    for line in file.read().splitlines():
+        sentence = (' '.join(line.split()))
+        entry = {"front": sentence, "back": translator.translate(
+            sentence, dest='es').text}
+        translations.append(entry)
     return translations
