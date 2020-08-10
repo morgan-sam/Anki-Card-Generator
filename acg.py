6d6b57f0538a41f7b7828a72580f85ad2af94847
Sam <samuel_morgan_@outlook.com>
2020-08-10 20:53:46 +0100

    Removed all env and key vars

--- acg.py
+++ acg.py
@@ -1,13 +1,7 @@
 from functions.generateCards import generateDeck
 from functions.listConversion import listToCardArray
 from functions.subtitlesConversion import subtitlesToCardArray
-from dotenv import load_dotenv, find_dotenv
-import os
-
-load_dotenv(find_dotenv())
-SECRET_KEY = os.environ.get("SECRET_KEY")
-print(SECRET_KEY)
 
 # cardArray = subtitlesToCardArray()
-cardArray = listToCardArray()
+# cardArray = listToCardArray()
 # generateDeck(cardArray)
