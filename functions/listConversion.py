7cde0473b5d3fa2723d3ca089017f855506e0837
Sam <samuel_morgan_@outlook.com>
2020-08-10 21:04:00 +0100

    Returns array of front/back objs

--- functions/listConversion.py
+++ functions/listConversion.py
@@ -8,5 +8,8 @@ def listToCardArray():
     filepath = 'Sentences.txt'
     with open(filepath) as fp:
         for line in enumerate(fp):
-            translations.append(translator.translate(line[1], dest='es').text)
+            entry = {"front": line[1], "back": translator.translate(
+                line[1], dest='es').text}
+            translations.append(entry)
     print(translations)
+    return translations
