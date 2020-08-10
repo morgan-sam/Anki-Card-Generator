d0f71d25603500380936a90db345a077a043dc0a
Sam <samuel_morgan_@outlook.com>
2020-08-10 21:29:23 +0100

    convertTextToMp3 fn working

--- acg.py
+++ acg.py
@@ -1,7 +1,9 @@
 from functions.generateCards import generateDeck
 from functions.listConversion import listToCardArray
 from functions.subtitlesConversion import subtitlesToCardArray
-
+from functions.convertTextToMp3 import convertTextToMp3
 # cardArray = subtitlesToCardArray()
-cardArray = listToCardArray()
+# cardArray = listToCardArray()
 # generateDeck(cardArray)
+
+convertTextToMp3('hey whats up')
---
+++ functions/convertTextToMp3.py
@@ -0,0 +1,29 @@
+
+from google.cloud import texttospeech
+import os
+
+
+def convertTextToMp3(inputText):
+    parent = os.path.abspath(os.path.join(__file__, '../..'))
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = parent + \
+        '/keys/acgApiKey.json'
+    client = texttospeech.TextToSpeechClient()
+
+    synthesis_input = texttospeech.SynthesisInput(
+        text=inputText)
+
+    voice = texttospeech.VoiceSelectionParams(
+        language_code='en-US',
+        name='en-US-Wavenet-C',
+        ssml_gender=texttospeech.SsmlVoiceGender.FEMALE)
+
+    audio_config = texttospeech.AudioConfig(
+        audio_encoding=texttospeech.AudioEncoding.MP3)
+
+    response = client.synthesize_speech(
+        input=synthesis_input, voice=voice, audio_config=audio_config
+    )
+
+    with open('output.mp3', 'wb') as out:
+        out.write(response.audio_content)
+    print('Created "output.mp3"')
