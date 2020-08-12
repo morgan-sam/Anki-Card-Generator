da5fd1c9a00a6d71bcbebce9676663aa775b2973
Sam <samuel_morgan_@outlook.com>
2020-08-12 16:27:23 +0100

    extractParagraphSentences file skeleton

--- acg.py
+++ acg.py
@@ -2,6 +2,10 @@ from bs4 import BeautifulSoup
 from functions.generateCards import generateDeck
 from functions.listConversion import listToCardArray
 from functions.subtitlesConversion import subtitlesToCardArray
+from functions.extractParagraphSentences import extractParagraphSentences
+
 # cardArray = subtitlesToCardArray()
 # cardArray = listToCardArray()
 # generateDeck(cardArray)
+
+extractParagraphSentences()
---
+++ functions/extractParagraphSentences.py
@@ -0,0 +1,4 @@
+
+
+def extractParagraphSentences():
+    print('hi')
