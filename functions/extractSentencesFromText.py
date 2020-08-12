e63e1d5ed1cd522936a2b860d218e822f914d78e
Sam <samuel_morgan_@outlook.com>
2020-08-12 16:46:16 +0100

    removeNumericCommas works

--- functions/extractSentencesFromText.py
+++ functions/extractSentencesFromText.py
@@ -1,4 +1,20 @@
+import re
+
+
+def removeCommas(string):
+    return string.replace(',', '')
+
+
+def removeNumericCommas(str):
+    return re.sub(r'[0-9]+(?:\,[0-9]+)+',
+                  lambda x: removeCommas(x[0]), str)
 
 
 def extractSentencesFromText():
-    print('hi')
+    file = open('text.txt')
+    for line in file.read().splitlines():
+        sentence = (' '.join(line.split()))
+        if sentence.endswith('.'):
+            sentence = sentence[:-1]
+        sentence = removeNumericCommas(sentence)
+        print(sentence)
