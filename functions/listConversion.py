f22edb049dc114d32db9197e48a0de86a084e408
Sam <samuel_morgan_@outlook.com>
2020-08-12 13:29:27 +0100

    Translation in var

--- functions/listConversion.py
+++ functions/listConversion.py
@@ -10,7 +10,7 @@ def listToCardArray():
         sentence = (' '.join(line.split()))
         if sentence.endswith('.'):
             sentence = sentence[:-1]
-        entry = {"front": sentence, "back": translator.translate(
-            sentence, dest='es').text}
+        translation = translator.translate(sentence, dest='es').text
+        entry = {"front": sentence, "back": translation}
         translations.append(entry)
     return translations
