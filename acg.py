262cd73e2adb07a6f63aa888e2e497a5e0121991
Sam <samuel_morgan_@outlook.com>
2020-08-12 16:20:53 +0100

    Get wikipedia page as text

--- acg.py
+++ acg.py
@@ -1,6 +1,17 @@
+from bs4 import BeautifulSoup
 from functions.generateCards import generateDeck
 from functions.listConversion import listToCardArray
 from functions.subtitlesConversion import subtitlesToCardArray
 # cardArray = subtitlesToCardArray()
-cardArray = listToCardArray()
-generateDeck(cardArray)
+# cardArray = listToCardArray()
+# generateDeck(cardArray)
+
+
+import urllib.request
+url = 'https://en.wikipedia.org/wiki/Snake'
+uf = urllib.request.urlopen(url)
+html = uf.read()
+
+soup = BeautifulSoup(html, 'html.parser')
+for p in soup.find_all('p'):
+    print(p.get_text() + "\n")
